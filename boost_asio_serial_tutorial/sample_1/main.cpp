7de97a3ab4375c0caa5b8d7e2c9c9ecccdb38073
Noble, Frazer <fknoble@massey.ac.nz>
2019-04-23 16:18:11 +1200

    Fixed an overwrite error.

--- boost_asio_serial_tutorial/sample_1/main.cpp
+++ boost_asio_serial_tutorial/sample_1/main.cpp
@@ -7,16 +7,46 @@ int main(int argc, char* argv[]) {
 
 		boost::asio::io_context io;
 
-		boost::asio::tcp::resolver resolver(io);
+		boost::asio::serial_port serial(io, "COM4");
 
-		boost::asio::tcp::resolver::results_type end_point = resolver.resolve(boost::asio::tcp::ip_address("127.0.0.1"));
+		serial.set_option(boost::asio::serial_port_base::baud_rate(115200));
+		serial.set_option(boost::asio::serial_port_base::character_size(8));
+		serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+		serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
 
-		
+		std::string data{ "Hello World!\r\n" };
+
+		boost::asio::write(serial, boost::asio::buffer(data, data.size()));
+
+		bool read{ true };
+
+		while (read) {
+
+			char c{ NULL };
+			boost::asio::read(serial, boost::asio::buffer(&c, 1));
+
+			switch (c) {
+			case '\r':
+				break;
+			case '\n':
+				read = false;
+				break;
+			default:
+				std::cout << c;
+			}
+
+		}
+
+		serial.close();
+
 	}
-	catch (boost::system::error_code &e) {
+	catch (boost::system::error_code & e) {
+
 		std::cout << e << std::endl;
+
 		return 1;
+
 	}
-	   
-    return 0;
+
+	return 0;
 }
